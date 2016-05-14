03ab7b2f0d5ee9e5bab2b4fc08cd8a6140721eb0
santy2509 <santosh.vy1144@gmail.com>
2016-05-13 17:33:01 -0700

    chef nginx receipe

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name             'my_cookbook'
+maintainer       'YOUR_COMPANY_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures test'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.2.9'
+depends		'apt'
+#depends		'java'
